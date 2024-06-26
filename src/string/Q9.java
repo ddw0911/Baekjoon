0c859dd4a55d6e7b960d89618c1c3dba143651f0
Dongwoo Jang <ddokkochi@gmail.com>
2024-06-26 17:54:27 +0900

    백준 문자열

--- src/string/Q9.java
+++ src/string/Q9.java
@@ -3,6 +3,7 @@ package string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Q9 {
@@ -10,15 +11,16 @@ public class Q9 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb1 = new StringBuilder();
+    sb1.append(st.nextToken());
+    sb1.reverse();
+    int num1 = Integer.parseInt(String.valueOf(sb1));
 
-    String input1 = st.nextToken();
-    String input2 = st.nextToken();
+    StringBuilder sb2 = new StringBuilder();
+    sb2.append(st.nextToken());
+    sb2.reverse();
+    int num2 = Integer.parseInt(String.valueOf(sb2));
 
-
-
-    input1 = input1;
-    input2 = input2.substring(2, 0);
-    System.out.println(input1);
-    System.out.println(input2);
+    System.out.println(Math.max(num1, num2));
   }
 }
